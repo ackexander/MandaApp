73a86f5c51233fc7a3c2364cfa6158b386a05a27
unknown <hansson@hansson-PC.(none)>
2012-02-08 00:00:15 +0100

    [Improvement] Added initial gui and some button listener stubs

--- src/dev/manda/nu/MandaAppActivity.java
+++ src/dev/manda/nu/MandaAppActivity.java
@@ -2,12 +2,30 @@ package dev.manda.nu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MandaAppActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	public void searchWiki(View view) {
+		System.out.println("wiki");
+	}
+
+	public void yakatanaRecipe(View view) {
+		System.out.println("yakatana");
+	}
+
+	public void songList(View view) {
+		System.out.println("songs");
+	}
+
+	public void imageGallery(View view) {
+		System.out.println("images");
+	}
 }
